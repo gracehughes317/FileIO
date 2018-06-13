0b7597682e65d8ab0532692294e606243d010783
Eric <ericychen99@gmail.com>
2018-06-13 14:25:31 -0500

    more sum method

--- SumAndList.cs
+++ SumAndList.cs
@@ -7,10 +7,20 @@ public class SumAndList
         int sum(byte[] file)
         {
             int count = 0;
-            for (int i = 0; i < file.ReadLines(file).Length; i++) {
-            count += number;
+            for (int i = 0; i < file.ReadLines(file).Length; i++)
+            {
+                count += number;
             }
             return count;
+        }
+
+        void displayList(byte[] file)
+        {
+            for (int i = 0; i < file.ReadLines(file).Length; i++)
+            {
+                Console.WriteLine(file.ReadLine)
+            }
+
         }
 	}
 }
