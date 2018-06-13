7ca129286c259e08cf63f1af3ed9485dea19377b
Vishnu <vishnuappalaraju@gmail.com>
2018-06-13 14:26:12 -0500

    evenodd

---
+++ evenodd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class evenodd
+{
+	public evensodd(List<Integer> x)
+	{
+        for(int i = 0; i < 0; i++)
+        {
+            if (((x.get(i)) % 2) == 0)
+            {
+                Console.WriteLine(x.get(i) + " Even");
+            }
+            if (((x.get(i)) % 2) != 0)
+            {
+                Console.WriteLine(x.get(i) + " Odd");
+            }
+        }
+	}
+}
