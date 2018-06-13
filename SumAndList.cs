ac4ac03a8d26a7b49098849ff5ea24cc689502b3
Eric <ericychen99@gmail.com>
2018-06-13 14:16:33 -0500

    Sum method

---
+++ SumAndList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class SumAndList
+{
+	public SumAndList()
+	{
+        int sum(byte[] file)
+        {
+            int count = 0;
+            for (int i = 0; i < file.ReadLines(file).Length; i++) {
+            count += number;
+            }
+            return count;
+        }
+	}
+}
