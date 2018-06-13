3ba40eae6bace8a8d70580c3e935be77a9eb6c20
Unknown <19brunel@elmbrookstudents.org>
2018-06-13 14:21:45 -0500

    Working on Stuff maybe works

---
+++ DataFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+public class DataFile
+{
+    private string winDir = System.Environment.GetEnvironmentVariable("windir");
+    private List<int> lines = new List<int>();
+
+    public DataFile() {
+
+    }
+    private void addItem(String value){
+        lines.add(Integer.parseInt(value));
+    }
+    private List<int> getList(String name){
+        StreamReader reader = new StreamReader(winDir + name);
+        try
+        {
+            do
+            {
+                addItem(reader.ReadLine());
+            }
+            while (reader.Peek() != -1);
+        }
+
+        catch
+        {
+            addListItem("File is empty");
+        }
+
+        finally
+        {
+            reader.Close();
+        }
+        return getList<int>;
+    }
+
+}
