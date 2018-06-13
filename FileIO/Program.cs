73bc49a92b754e9630bcaf0377edc412a8d6ea4c
Unknown <gracehughes317@gmail.com>
2018-06-13 14:26:14 -0500

    more methods added

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows;
+using System.IO;
 
 namespace FileIO
 {
@@ -6,9 +8,15 @@ namespace FileIO
     {
         static void Main(string[] args)
         {
+            //stack overflow below
+            string winDir = System.Environment.GetEnvironmentVariable("windir");
+            StreamReader reader = new StreamReader(winDir + "\\system.ini");
+
             Console.WriteLine("Name of data file");
             string fileName = Console.ReadLine();
 
+            //method that retrieves the next line
+            //reader.ReadLine();
         }
     }
 }
