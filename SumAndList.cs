a07aec9d6394962d9b84f61f577f191f05fb29df
Eric <ericychen99@gmail.com>
2018-06-14 09:01:55 -0500

    formatting

---
+++ SumAndList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public class SumAndList
+{
+    public SumAndList()
+    {
+    }
+
+        int sum(List<int> intList)
+        {
+            int count = 0;
+            foreach (int number in intList)
+            {
+                count += number;
+            }
+            return count;
+        }
+
+        void displayList(List<int> intList)
+        {
+            foreach (int number in intList)
+            {
+                Console.WriteLine(number);
+            }
+	    }
+}
